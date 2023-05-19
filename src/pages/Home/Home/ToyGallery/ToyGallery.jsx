f45aaaa84ff6961ebb31f578587a2f6f26bb46b5
hafizur27 <mahmadhafiz33@gmail.com>
2023-05-20 02:04:00 +0600

    toy gallery and firebase initialize done

---
+++ src/pages/Home/Home/ToyGallery/ToyGallery.jsx
@@ -0,0 +1,46 @@
+import Lottie from "lottie-react";
+import toy1 from "../../../../assets/toy/toy1.json";
+import toy2 from "../../../../assets/toy/toy2.json";
+import toy3 from "../../../../assets/toy/toy3.json";
+import toy4 from "../../../../assets/toy/toy4.json";
+import toy5 from "../../../../assets/toy/toy5.json";
+import toy6 from "../../../../assets/toy/toy6.json";
+
+const ToyGallery = () => {
+  return (
+    <div className="mt-4 mb-10">
+      <h2 className="text-4xl font-bold text-center text-teal-400">
+        Toy Gallery
+      </h2>
+      
+        <div className="card w-full shadow-xl grid grid-col-1 md:grid-cols-3 space-x-4 space-y-4">
+        <div className="px-10 pt-10 shadow-xl rounded">
+            <Lottie animationData={toy4} loop={true} className="" />
+          </div>
+        <div className="px-10 pt-10 shadow-xl rounded">
+            <Lottie animationData={toy3} loop={true} className="" />
+          </div>
+          
+          <div className="px-10 pt-10 shadow-xl rounded">
+            <Lottie animationData={toy2} loop={true} className="" />
+          </div>
+          
+         
+          <div className="px-10 pt-10 shadow-xl rounded">
+            <Lottie animationData={toy5} loop={true} className="" />
+          </div>
+          <div className="px-10 pt-10 shadow-xl rounded">
+            <Lottie animationData={toy6} loop={true} className="" />
+            
+          </div>
+          <div className="px-10 pt-10 shadow-xl rounded">
+            <Lottie animationData={toy1} loop={true} className="" />
+          </div>
+        
+        </div>
+      
+    </div>
+  );
+};
+
+export default ToyGallery;
