53994a6c2f6d6920ebc7534c983f12d5703efb9b
hafizur27 <mahmadhafiz33@gmail.com>
2023-05-21 13:32:16 +0600

    all toy page search toy initialization

--- src/pages/AllToys/AllToys.jsx
+++ src/pages/AllToys/AllToys.jsx
@@ -1,13 +1,25 @@
 
 import { useLoaderData } from "react-router-dom";
 import ToyRow from "./ToyRow";
+import { useState } from "react";
 
 
 
 
 const AllToys = () => {
   
-  const allToys = useLoaderData();
+  const loadedAllToys = useLoaderData();
+  const [allToys, SetAllToys] = useState(loadedAllToys);
+  const [searchText, setSearchText] = useState('');
+
+  const handelToySearch = () =>{
+    fetch(`http://localhost:5000/searchToyByText/${searchText}`)
+    .then(res => res.json())
+    .then(data => {
+      SetAllToys(data);
+    })
+  }
+  
 
   
   return (
@@ -15,6 +27,10 @@ const AllToys = () => {
       <h3 className="text-3xl font-bold text-teal-500 mb-8">
         All crazy cars Information
       </h3>
+      <div className="mb-4">
+      <input onChange={(e) => setSearchText(e.target.value)} type="text" placeholder="Search toy here" className="input input-bordered w-full max-w-xs" />
+      <button onClick={handelToySearch} className="btn btn-outline ml-2">Search</button>
+      </div>
       <table className="table table-zebra w-full">
         <thead>
           <tr>
--- src/pages/Update/Update.jsx
+++ src/pages/Update/Update.jsx
@@ -1,11 +1,79 @@
-
+import update from "../../assets/update.json";
+import Lottie from "lottie-react";
 
 const Update = () => {
-    return (
-        <div>
-            sggrsg
+    const handelUpdate = event =>{
+        event.preventDefault();
+        const form = event.target;
+        const price = form.price.value;
+        const quantity = form.quantity.value;
+        const description = form.description.value;
+        const updateUser = {price, quantity, description};
+        console.log(updateUser);
+
+
+    };
+  return (
+    <div>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="text-center lg:text-left">
+            <div className="px-10 pt-10 shadow-xl rounded">
+              <Lottie animationData={update} loop={true} className="" />
+            </div>
+          </div>
+          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <div className="card-body">
+              <h3 className="text-xl font-bold">please update now !!</h3>
+              <hr/>
+              <form onSubmit={handelUpdate}>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Price</span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Update price"
+                    name="price"
+                    className="input input-bordered"
+                  />
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Quantity</span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Update quantity"
+                    name="quantity"
+                    className="input input-bordered"
+                  />
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="label-text">Description</span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Update description"
+                    name="description"
+                    className="input input-bordered"
+                  />
+                </div>
+                <div className="form-control mt-6">
+                  <input
+                    type="submit"
+                    value="update now"
+                    className="btn btn-outline btn-warning"
+                  />
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Update;
